cf0886dccc457515019d7d7d9be7c290d8171848
izzuddin <izzuddin@petbacker.com>
2025-03-17 16:16:13 +0800

    setting up server response

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,10 +1,26 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets"
-
+import { OnModuleInit } from "@nestjs/common"
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets"
+import { Server } from 'socket.io'
 @WebSocketGateway()
-export class MyGateway {
+export class MyGateway implements OnModuleInit {
+    @WebSocketServer()
+
+    server: Server
+
+    onModuleInit () {
+        this.server.on('connection', (socket) => {
+            console.log(socket.id)
+            console.log('Connected')
+        })
+    }
 
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body:any){
+        this.server.emit('onMessage',{
+            msg:'New Message',
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            content: body
+        })
         console.log(body)
     }
     @SubscribeMessage('newChat')
